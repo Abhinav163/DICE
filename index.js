b7a6febc3198af7d38f241c5f6f8b7e9e9482ca8
Abhinav Sharma <113231995+Abhinav163@users.noreply.github.com>
2024-02-22 12:47:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 function random(){
         var randomnum = Math.floor(Math.random()*6)+1;
         if(randomnum===1){
-        document.getElementById("img2").src="./images/dice1.png";
+        document.getElementById("img2").src="./dice1.png";
         }
         else if(randomnum===2){
-        document.getElementById("img2").src="./images/dice2.png";
+        document.getElementById("img2").src="./dice2.png";
         }
         else if(randomnum===3){
-        document.getElementById("img2").src="./images/dice3.png";
+        document.getElementById("img2").src="./dice3.png";
         }
         else if(randomnum===4){
-        document.getElementById("img2").src="./images/dice4.png";
+        document.getElementById("img2").src="./dice4.png";
         }
         else if(randomnum===5){
-        document.getElementById("img2").src="./images/dice5.png";
+        document.getElementById("img2").src="./dice5.png";
         }
         else if(randomnum===6){
-        document.getElementById("img2").src="./images/dice6.png";
+        document.getElementById("img2").src="./dice6.png";
         }
 }
